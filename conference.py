ec99346cda0c2ebb676f204790b6e384cf24cc70
emguy <emguy.yuzhang@gmail.com>
2016-02-18 15:02:36 -0500

    new features are implemented

--- conference.py
+++ conference.py
@@ -9,10 +9,9 @@ $Id: conference.py,v 1.25 2014/05/24 23:42:19 wesc Exp wesc $
 created by wesc on 2014 apr 21
 
 """
+__author__ = "wesc+api@google.com (Wesley Chun)"
 
-__author__ = 'wesc+api@google.com (Wesley Chun)'
-
-
+import logging
 from datetime import datetime
 import json
 import os
@@ -30,94 +29,205 @@ from models import Profile
 from models import ProfileMiniForm
 from models import ProfileForm
 from models import TeeShirtSize
+from models import Conference
+from models import ConferenceForm
+from models import ConferenceForms
+from models import ConferenceQueryForm
+from models import ConferenceQueryForms
 
 from settings import WEB_CLIENT_ID
+from  utils import getUserId
+
+DEFAULTS = {
+    "city": "Default City",
+    "maxAttendees": 0,
+    "seatsAvailable": 0,
+    "topics": [ "Default", "Topic" ],
+}
 
 EMAIL_SCOPE = endpoints.EMAIL_SCOPE
 API_EXPLORER_CLIENT_ID = endpoints.API_EXPLORER_CLIENT_ID
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-@endpoints.api( name='conference',
-                version='v1',
+@endpoints.api( name="conference",
+                version="v1",
                 allowed_client_ids=[WEB_CLIENT_ID, API_EXPLORER_CLIENT_ID],
                 scopes=[EMAIL_SCOPE])
 class ConferenceApi(remote.Service):
-    """Conference API v0.1"""
-
+  """Conference API v0.1"""
 # - - - Profile objects - - - - - - - - - - - - - - - - - - -
+  def _copyProfileToForm(self, prof):
+    """Copy relevant fields from Profile to ProfileForm."""
+    # copy relevant fields from Profile to ProfileForm
+    pf = ProfileForm()
+    for field in pf.all_fields():
+      if hasattr(prof, field.name):
+        # convert t-shirt string to Enum; just copy others
+        if field.name == "teeShirtSize":
+          setattr(pf, field.name, getattr(TeeShirtSize, getattr(prof, field.name)))
+        else:
+          setattr(pf, field.name, getattr(prof, field.name))
+    pf.check_initialized()
+    return pf
 
-    def _copyProfileToForm(self, prof):
-        """Copy relevant fields from Profile to ProfileForm."""
-        # copy relevant fields from Profile to ProfileForm
-        pf = ProfileForm()
-        for field in pf.all_fields():
-            if hasattr(prof, field.name):
-                # convert t-shirt string to Enum; just copy others
-                if field.name == 'teeShirtSize':
-                    setattr(pf, field.name, getattr(TeeShirtSize, getattr(prof, field.name)))
-                else:
-                    setattr(pf, field.name, getattr(prof, field.name))
-        pf.check_initialized()
-        return pf
+  def _getProfileFromUser(self):
+    """Return user Profile from datastore, creating new one if non-existent."""
+    user = endpoints.get_current_user()
+    if not user:
+         raise endpoints.UnauthorizedException("Authorization required")
+    # get Profile from database
+    user_id = getUserId(user, id_type="oauth")
+    p_key = ndb.Key(Profile, user_id)
+    profile = p_key.get() # this creates a profile object
+    # create new Profile if not there
+    if not profile:
+      profile = Profile(
+        key = p_key,
+        displayName = user.nickname(), 
+        mainEmail= user.email(),
+        teeShirtSize = str(TeeShirtSize.NOT_SPECIFIED),
+      )
+      profile.put() # this place the profile object on google cloud datastore
+    return profile      # return Profile
 
+  def _doProfile(self, save_request=None):
+    """Get user Profile and return to user, possibly updating it first."""
+    # get user Profile
+    prof = self._getProfileFromUser()
 
-    def _getProfileFromUser(self):
-        """Return user Profile from datastore, creating new one if non-existent."""
-        ## TODO 2
-        ## step 1: make sure user is authed
-        ## uncomment the following lines:
-        # user = endpoints.get_current_user()
-        # if not user:
-        #     raise endpoints.UnauthorizedException('Authorization required')
-        profile = None
-        ## step 2: create a new Profile from logged in user data
-        ## you can use user.nickname() to get displayName
-        ## and user.email() to get mainEmail
-        if not profile:
-            profile = Profile(
-                userId = None,
-                key = None,
-                displayName = "Test", 
-                mainEmail= None,
-                teeShirtSize = str(TeeShirtSize.NOT_SPECIFIED),
-            )
+    # if saveProfile(), process user-modifyable fields
+    if save_request:
+      for field in ("displayName", "teeShirtSize"):
+        if hasattr(save_request, field):
+          val = getattr(save_request, field)
+          if val:
+            setattr(prof, field, str(val))
+    # 
+    prof.put()
+    # return ProfileForm
+    return self._copyProfileToForm(prof)
 
-        return profile      # return Profile
+# - - - Conference objects - - - - - - - - - - - - - - - - -
+  def _copyConferenceToForm(self, conf, displayName):
+    """Copy relevant fields from Conference to ConferenceForm."""
+    cf = ConferenceForm()
+    for field in cf.all_fields():
+      if hasattr(conf, field.name):
+        # convert Date to date string; just copy others
+        if field.name.endswith('Date'):
+          setattr(cf, field.name, str(getattr(conf, field.name)))
+        else:
+          setattr(cf, field.name, getattr(conf, field.name))
+      elif field.name == "websafeKey":
+        setattr(cf, field.name, conf.key.urlsafe())
+    if displayName:
+      setattr(cf, 'organizerDisplayName', displayName)
+    cf.check_initialized()
+    return cf
 
+  def _createConferenceObject(self, request):
+    """Create or update Conference object, returning ConferenceForm/request."""
+    # preload necessary data items
+    user = endpoints.get_current_user()
+    if not user:
+      raise endpoints.UnauthorizedException('Authorization required')
+    user_id = getUserId(user)
 
-    def _doProfile(self, save_request=None):
-        """Get user Profile and return to user, possibly updating it first."""
-        # get user Profile
-        prof = self._getProfileFromUser()
+    if not request.name:
+      raise endpoints.BadRequestException("Conference 'name' field required")
 
-        # if saveProfile(), process user-modifyable fields
-        if save_request:
-            for field in ('displayName', 'teeShirtSize'):
-                if hasattr(save_request, field):
-                    val = getattr(save_request, field)
-                    if val:
-                        setattr(prof, field, str(val))
+    # copy ConferenceForm/ProtoRPC Message into dict
+    data = {field.name: getattr(request, field.name) for field in request.all_fields()}
+    del data['websafeKey']
+    del data['organizerDisplayName']
 
-        # return ProfileForm
-        return self._copyProfileToForm(prof)
+    # add default values for those missing (both data model & outbound Message)
+    for df in DEFAULTS:
+      if data[df] in (None, []):
+        data[df] = DEFAULTS[df]
+        setattr(request, df, DEFAULTS[df])
 
+    # convert dates from strings to Date objects; set month based on start_date
+    if data['startDate']:
+      data['startDate'] = datetime.strptime(data['startDate'][:10], "%Y-%m-%d").date()
+      data['month'] = data['startDate'].month
+    else:
+      data['month'] = 0
+    if data['endDate']:
+      data['endDate'] = datetime.strptime(data['endDate'][:10], "%Y-%m-%d").date()
 
-    @endpoints.method(message_types.VoidMessage, ProfileForm,
-            path='profile', http_method='GET', name='getProfile')
-    def getProfile(self, request):
-        """Return user profile."""
-        return self._doProfile()
+    # set seatsAvailable to be same as maxAttendees on creation
+    # both for data model & outbound Message
+    if data["maxAttendees"] > 0:
+      data["seatsAvailable"] = data["maxAttendees"]
+      setattr(request, "seatsAvailable", data["maxAttendees"])
 
-    # TODO 1
-    # 1. change request class
-    # 2. pass request to _doProfile function
-    @endpoints.method(message_types.VoidMessage, ProfileForm,
-            path='profile', http_method='POST', name='saveProfile')
-    def saveProfile(self, request):
-        """Update & return user profile."""
-        return self._doProfile()
+    # make Profile Key from user ID
+    p_key = ndb.Key(Profile, user_id)
+    # allocate new Conference ID with Profile key as parent
+    c_id = Conference.allocate_ids(size=1, parent=p_key)[0]
+    # make Conference key from ID
+    c_key = ndb.Key(Conference, c_id, parent=p_key)
+    data['key'] = c_key
+    data['organizerUserId'] = request.organizerUserId = user_id
 
+    # create Conference & return (modified) ConferenceForm
+    Conference(**data).put()
+
+    return request
+
+  @endpoints.method(message_types.VoidMessage, ProfileForm,
+          path="profile", http_method="GET", name="getProfile")
+  def getProfile(self, request):
+    """Return user profile."""
+    return self._doProfile()
+
+  @endpoints.method(ProfileMiniForm, ProfileForm, path="profile", 
+            http_method="POST", name="saveProfile")
+  def saveProfile(self, request):
+    """Update & return user profile."""
+    return self._doProfile(request)
+
+  @endpoints.method(ConferenceForm, ConferenceForm, path='conference',
+          http_method='POST', name='createConference')
+  def createConference(self, request):
+    """Create new conference."""
+    return self._createConferenceObject(request)
+
+  @endpoints.method(ConferenceQueryForms, ConferenceForms,
+              path='queryConferences', http_method='POST',
+              name='queryConferences')
+  def queryConferences(self, request):
+    """Query for conferences."""
+    conferences = Conference.query()
+     # return individual ConferenceForm object per Conference
+    return ConferenceForms(
+      items=[self._copyConferenceToForm(conf, "") for conf in conferences]
+    )
+
+  @endpoints.method(message_types.VoidMessage, ConferenceForms,
+          path='getConferencesCreated',
+          http_method='POST', name='getConferencesCreated')
+  def getConferencesCreated(self, request):
+    """Return conferences created by user."""
+    # make sure user is authed
+    #user = endpoints.get_current_user()
+    #if not user:
+    #  raise endpoints.UnauthorizedException('Authorization required')
+  
+    # make profile key
+    #p_key = ndb.Key(Profile, getUserId(user))
+    # create ancestor query for this user
+    # conferences = Conference.query(ancestor=p_key)
+    conferences = Conference.query()
+    # get the user profile and display name
+    # prof = p_key.get()
+    # displayName = getattr(prof, 'displayName')
+    # return set of ConferenceForm objects per Conference
+    return ConferenceForms(
+      items=[self._copyConferenceToForm(conf, "") for conf in conferences]
+    )
 
 # registers API
 api = endpoints.api_server([ConferenceApi]) 
--- models.py
+++ models.py
@@ -19,41 +19,81 @@ from google.appengine.ext import ndb
 
 
 class Profile(ndb.Model):
-    """Profile -- User profile object"""
-    userId = ndb.StringProperty()
-    displayName = ndb.StringProperty()
-    mainEmail = ndb.StringProperty()
-    teeShirtSize = ndb.StringProperty(default='NOT_SPECIFIED')
-
+  """Profile -- User profile object"""
+  userId = ndb.StringProperty()
+  displayName = ndb.StringProperty()
+  mainEmail = ndb.StringProperty()
+  teeShirtSize = ndb.StringProperty(default='NOT_SPECIFIED')
 
 class ProfileMiniForm(messages.Message):
-    """ProfileMiniForm -- update Profile form message"""
-    displayName = messages.StringField(1)
-    teeShirtSize = messages.EnumField('TeeShirtSize', 2)
-
+  """ProfileMiniForm -- update Profile form message"""
+  displayName = messages.StringField(1)
+  teeShirtSize = messages.EnumField('TeeShirtSize', 2)
 
 class ProfileForm(messages.Message):
-    """ProfileForm -- Profile outbound form message"""
-    userId = messages.StringField(1)
-    displayName = messages.StringField(2)
-    mainEmail = messages.StringField(3)
-    teeShirtSize = messages.EnumField('TeeShirtSize', 4)
-
+  """ProfileForm -- Profile outbound form message"""
+  userId = messages.StringField(1)
+  displayName = messages.StringField(2)
+  mainEmail = messages.StringField(3)
+  teeShirtSize = messages.EnumField('TeeShirtSize', 4)
 
 class TeeShirtSize(messages.Enum):
-    """TeeShirtSize -- t-shirt size enumeration value"""
-    NOT_SPECIFIED = 1
-    XS_M = 2
-    XS_W = 3
-    S_M = 4
-    S_W = 5
-    M_M = 6
-    M_W = 7
-    L_M = 8
-    L_W = 9
-    XL_M = 10
-    XL_W = 11
-    XXL_M = 12
-    XXL_W = 13
-    XXXL_M = 14
-    XXXL_W = 15
+  """TeeShirtSize -- t-shirt size enumeration value"""
+  NOT_SPECIFIED = 1
+  XS_M = 2
+  XS_W = 3
+  S_M = 4
+  S_W = 5
+  M_M = 6
+  M_W = 7
+  L_M = 8
+  L_W = 9
+  XL_M = 10
+  XL_W = 11
+  XXL_M = 12
+  XXL_W = 13
+  XXXL_M = 14
+  XXXL_W = 15
+
+class Conference(ndb.Model):
+  """Conference -- Conference object"""
+  name            = ndb.StringProperty(required=True)
+  description     = ndb.StringProperty()
+  organizerUserId = ndb.StringProperty()
+  topics          = ndb.StringProperty(repeated=True)
+  city            = ndb.StringProperty()
+  startDate       = ndb.DateProperty()
+  month           = ndb.IntegerProperty()
+  endDate         = ndb.DateProperty()
+  maxAttendees    = ndb.IntegerProperty()
+  seatsAvailable  = ndb.IntegerProperty()
+
+class ConferenceForm(messages.Message):
+  """ConferenceForm -- Conference outbound form message"""
+  name            = messages.StringField(1)
+  description     = messages.StringField(2)
+  organizerUserId = messages.StringField(3)
+  topics          = messages.StringField(4, repeated=True)
+  city            = messages.StringField(5)
+  startDate       = messages.StringField(6)
+  month           = messages.IntegerField(7, variant=messages.Variant.INT32)
+  maxAttendees    = messages.IntegerField(8, variant=messages.Variant.INT32)
+  seatsAvailable  = messages.IntegerField(9, variant=messages.Variant.INT32)
+  endDate         = messages.StringField(10)
+  websafeKey      = messages.StringField(11)
+  organizerDisplayName = messages.StringField(12)
+
+class ConferenceForms(messages.Message):
+  """ConferenceForms -- multiple Conference outbound form message"""
+  items = messages.MessageField(ConferenceForm, 1, repeated=True)
+
+class ConferenceQueryForm(messages.Message):
+  """ConferenceQueryForm -- Conference query inbound form message"""
+  field = messages.StringField(1)
+  operator = messages.StringField(2)
+  value = messages.StringField(3)
+
+class ConferenceQueryForms(messages.Message):
+  """ConferenceQueryForms -- multiple ConferenceQueryForm inbound form message"""
+  filters = messages.MessageField(ConferenceQueryForm, 1, repeated=True)
+
