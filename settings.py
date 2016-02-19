18e316661ba260b2ee6c1f6366cda7daa433d0dc
emguy <emguy.yuzhang@gmail.com>
2016-02-19 12:51:05 -0500

    settings file is included.

--- settings.py
+++ settings.py
@@ -12,5 +12,4 @@ created/forked from conference.py by wesc on 2014 may 24
 
 # Replace the following lines with client IDs obtained from the APIs
 # Console or Cloud Console.
-WEB_CLIENT_ID = 'replace with Web client ID'
-
+WEB_CLIENT_ID = '1030942758608-ufjc7oer9vf2i911msgr80jooodrachs.apps.googleusercontent.com'
